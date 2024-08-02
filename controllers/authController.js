24da4fa748fa3bf4a6b30057a0966df6c656256d
arispretz <117314460+arispretz@users.noreply.github.com>
2024-08-01 21:52:25 -0300

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
+const register = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, password: hashedPassword });
+        await user.save();
+        res.status(201).send('User registered');
+    } catch (error) {
+        res.status(400).send('Error registering user');
+    }
+};
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).send('Invalid credentials');
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+};
+
+module.exports = { register, login };
